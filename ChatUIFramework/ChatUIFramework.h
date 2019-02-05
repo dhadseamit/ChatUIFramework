80c28e5ea1ebb08a14c53318217f02a770b395ea
Amit <a83@Amits-Mac.local>
2019-02-05 13:22:42 +0530

    First commit

---
+++ ChatUIFramework/ChatUIFramework.h
@@ -0,0 +1,19 @@
+//
+//  ChatUIFramework.h
+//  ChatUIFramework
+//
+//  Created by Amit on 05/02/19.
+//  Copyright © 2019 Josh Software. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ChatUIFramework.
+FOUNDATION_EXPORT double ChatUIFrameworkVersionNumber;
+
+//! Project version string for ChatUIFramework.
+FOUNDATION_EXPORT const unsigned char ChatUIFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ChatUIFramework/PublicHeader.h>
+
+
---
+++ ChatUIFramework/Service.swift
@@ -0,0 +1,19 @@
+//
+//  Service.swift
+//  ChatUIFramework
+//
+//  Created by Amit on 05/02/19.
+//  Copyright © 2019 Josh Software. All rights reserved.
+//
+
+import Foundation
+
+public class Service{
+    
+    private init(){}
+    
+    public static func getService() -> String    {
+
+        return "Fucker"
+    }
+}
